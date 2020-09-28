26a414a8f41f6c47a497390ee67d8665ecfcb1e9
Alfonso <alfof87@gmail.com>
2020-09-28 17:25:48 +0200

    laravel-base-crud

--- esercizio28.09.2020/resources/views/layouts/pagamento-edit.blade.php
+++ esercizio28.09.2020/resources/views/layouts/pagamento-edit.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.main-layout');
 @section('section')
   <h1>PAGAMENTO EDIT</h1>
+  <br>
   <form class="" action="{{route('pagamento.update', $pagamento -> id)}}" method="post">
     @csrf
     @method('POST')
-    <div class="">
+    <div class="input">
       <label for="status">STATUS</label>
       <input type="text" name="status" value="{{ $pagamento -> status }}">
     </div>
-    <div class="">
+    <br>
+    <div class="input">
       <label for="price">PRICE</label>
       <input type="number" name="price" value="{{ $pagamento -> price }}">
     </div>
-
-    <button type="submit" name="button">UPDATE</button>
+    <br>
   </form>
+  <button type="submit" name="button">UPDATE</button>
 @endsection
